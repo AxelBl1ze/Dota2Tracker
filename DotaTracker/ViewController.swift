61a3125fec91c2024ac0932e4abc92412734b764
Ilya Sidnev <AxelBlazeee@yandex.ru>
2025-01-31 22:35:48 +0500

    Initial Commit

---
+++ DotaTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DotaTracker
+//
+//  Created by Ilya Sidnev on 1/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
